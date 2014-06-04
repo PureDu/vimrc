02f4fa44f6aff3f4a684d0321d89dad2e5a4803e
duxiaobo <duxiaobo@code2living.www.code2living.com>
2014-06-05 07:36:12 +0800

    second commit

---
+++ ln.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+BASEDIR=$(dirname $0)
+cd $BASEDIR
+CURRENT_DIR=`pwd`
+
+lnif() {
+    if [ -e "$1" ]; then
+        ln -sf "$1" "$2"
+    fi
+}
+
+echo "Step1: backing up current vim config"
+today=`date +%Y%m%d`
+for i in $HOME/.vim $HOME/.vimrc $HOME/.gvimrc; do [ -e $i ] && [ ! -L $i ] && mv $i $i.$today; done
+for i in $HOME/.vim $HOME/.vimrc $HOME/.gvimrc; do [ -L $i ] && unlink $i ; done
+
+
+echo "Step2: setting up symlinks"
+lnif $CURRENT_DIR/vimrc $HOME/.vimrc
+lnif $CURRENT_DIR/vimrc.bundles $HOME/.vimrc.bundles
+lnif "$CURRENT_DIR/" "$HOME/.vim"
